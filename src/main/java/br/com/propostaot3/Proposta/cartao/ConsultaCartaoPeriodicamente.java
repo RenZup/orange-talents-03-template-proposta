b1f5361b44d67d12d5a0b3364f49e6452f872e69
Renzo Marquez Piedade <renzo.piedade@zup.com.br>
2021-05-03 14:34:20 -0300

    Ajustes e remoção do endpoint de cadastro de cartoes

--- src/main/java/br/com/propostaot3/Proposta/cartao/ConsultaCartaoPeriodicamente.java
+++ src/main/java/br/com/propostaot3/Proposta/cartao/ConsultaCartaoPeriodicamente.java
@@ -1,12 +1,9 @@
 package br.com.propostaot3.Proposta.cartao;
 
-import br.com.propostaot3.Proposta.cartao.consultaCartao.ConsultaCartao;
-import br.com.propostaot3.Proposta.cartao.consultaCartao.ConsultaCartaoResponse;
 import br.com.propostaot3.Proposta.proposta.Proposta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,13 +14,9 @@ import java.util.List;
 public class ConsultaCartaoPeriodicamente {
 
     @PersistenceContext
-    EntityManager em;
-
+    private EntityManager em;
     @Autowired
-    ConsultaCartao consultaCartao;
-
-    @Autowired
-    CartaoController controller;
+    CriadorDeCartao criadorDeCartao;
 
     @Scheduled(fixedDelay = 30000)
     public void consulta(){
@@ -32,11 +25,9 @@ public class ConsultaCartaoPeriodicamente {
 
         for (Proposta p :
                 resultList) {
-            ConsultaCartaoResponse response = consultaCartao.consultar(new CartaoRequestForm());
-            controller.criar(new CartaoRequestForm(p.getId()), UriComponentsBuilder.newInstance());
+         criadorDeCartao.criaCartao(p.getId());
         }
 
-        System.out.println(resultList.toString());
 
     }
 }
